5af2ff7b44154ad0b2ca065288b05ad31570c4d7
DavidGomezGonzalez <61864620+DavidGomezGonzalez@users.noreply.github.com>
2021-07-10 02:20:11 +0200

    Primer Version

---
+++ role.harvester.js
@@ -0,0 +1,39 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.store.getFreeCapacity() > 0) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else {
+                creep.say('🔄FULL');
+                var source = Game.getObjectById('00121f043e2c64c');
+                // move my creep to the energy source and harvest energy
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffffff'}});
+                /*if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                }*/
+                var newName = 'Upgrader' + Game.time;
+                var res = Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE], newName,
+                    {memory: {role: 'upgrader'}});
+
+            }
+      }
+	}
+};
+
+module.exports = roleHarvester;
